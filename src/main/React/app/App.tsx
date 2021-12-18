1cb4b7a55a2441b3f398502c77bd7fe9352b1d11
layout-IT <77637040+layout-IT@users.noreply.github.com>
2021-12-18 14:56:21 +0300

    Оптимизация импортов

--- src/main/React/app/App.tsx
+++ src/main/React/app/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import {Route, Routes} from 'react-router-dom'
 import s from './App.module.scss';
 import RoutesList from '../RoutesList'
 import Header from '../header/Header';
 
 export const App = () => {
-  return (
-      <div className={s.wrapper}>
-        <Header/>
-        <RoutesList/>
-      </div>
-  );
+    return (
+        <div className={s.wrapper}>
+            <Header/>
+            <RoutesList/>
+        </div>
+    );
 }
