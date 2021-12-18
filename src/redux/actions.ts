e522476aa46d84b82e413a81fbd4d72cf71b3dd6
NM-va <nad.n.mwa@gmail.com>
2021-12-18 15:57:14 +0300

    add actions,  add reducer in store

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,7 +1,18 @@
 import { cardsReducer } from './reducers/cardsReducer';
+import { AddedUserType } from './reducers/registrationReducer';
 
 
 export const action1 = (value: any) => ({ type: 'cardsReducer/NEW_ARRAY_CARDS'} as const);
 
+export const setUserAC = (user: AddedUserType) => ({type: 'cardsReducer/SET-USER', user} as const);
+export const setIsRegisteredAC = (isRegistered: boolean) => ({type: 'cardsReducer/SET-IS-REGISTERED', isRegistered} as const);
+
+
+//types
+export type setUserActionType = ReturnType<typeof setUserAC>;
+export type setIsRegisteredType = ReturnType<typeof setIsRegisteredAC>;
+
 export type ActionTypes =
         | ReturnType<typeof action1>
+        | setIsRegisteredType
+        | setUserActionType
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,8 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunkMiddleware from "redux-thunk"
 import { cardsReducer } from "./reducers/cardsReducer";
+import {registrationReducer} from "./reducers/registrationReducer";
+
 
 
 
@@ -11,6 +13,7 @@ export const rootReducer = combineReducers({
   cardsPage: cardsReducer,
   // dialogsPage: dialogsReducer,
   // sidebarPage: sidebarReducer,
+  registrationPage: registrationReducer
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
