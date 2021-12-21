d38cd49e0f679db73b7a660cda1187436cd34ab0
NM-va <nad.n.mwa@gmail.com>
2021-12-21 23:59:34 +0300

    delete actions from common file, delete error-util file

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -3,15 +3,8 @@ import { AddedUserType } from './reducers/registrationReducer';
 
 export const action1 = (value: any) => ({ type: 'cardsReducer/NEW_ARRAY_CARDS'} as const);
 
-export const setUserAC = (user: AddedUserType) => ({type: 'cardsReducer/SET-USER', user} as const);
-export const setIsRegisteredAC = (isRegistered: boolean) => ({type: 'cardsReducer/SET-IS-REGISTERED', isRegistered} as const);
 
 
-//types
-export type setUserActionType = ReturnType<typeof setUserAC>;
-export type setIsRegisteredType = ReturnType<typeof setIsRegisteredAC>;
 
 export type ActionTypes =
         | ReturnType<typeof action1>
-        | setIsRegisteredType
-        | setUserActionType
